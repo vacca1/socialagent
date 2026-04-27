6e9d43e11388da582a27d4968e5fa7555553bd2f
konectaaai <konecta.ai@gmail.com>
2026-04-27 09:44:22 -0300

    Ignore TS and ESLint errors during Next.js build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   transpilePackages: ["@postador/types"],
 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "@postador/database"],
   },
